14cc6c61bacbf063c22ddd884d739881772fd0d3
Cjaydeep248 <cjaydeep248@gmail.com>
2024-03-11 11:39:42 +0530

    Enquiry (Micro Service) Structure is added

---
+++ HomeLoanProject_EurekaServer/src/test/java/com/homeloan/main/eurekaserver/HomeLoanProjectEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homeloan.main.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeLoanProjectEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
